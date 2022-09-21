7303f06091409fbcf757c3f1457f1ad5491c2319
Galileo Guilherme <galileoguilherme@outlook.com>
2022-09-21 00:10:03 -0300

    update

---
+++ automacao_ui/cypress/e2e/login.spec.js
@@ -0,0 +1,16 @@
+/// <reference types="cypress" />
+
+context('Funcionalidade Login', () => {
+
+    it('Deve fazer login com sucesso' , () =>{
+
+    })
+
+    it('Deve exibir um mensagem de erro ao inserir usuário inválido', () => {
+        
+    });
+
+    it('Deve exibir um mensagem de erro ao inserir senha inválida', () => {
+        
+    });
+});
